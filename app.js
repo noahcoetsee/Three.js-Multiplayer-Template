9888b82767725e0f5adbd12f6fd2330c2f17ce8d
Noah <noahcoetsee@gmail.com>
2017-09-14 17:11:08 -0400

    Began adding socket events

--- app.js
+++ app.js
@@ -13,8 +13,38 @@ var config = require('./config.json');
 /* Routing */
 app.use(express.static(__dirname + '/public'));
 
+/* Variables */
+var objects = [];
 
 /* Logic */
 io.on('connection', function (socket) {
-  
+  setupEvents(socket);
 });
+
+function setupEvents(socket) {
+  // Connection Events
+  socket.on('new player', function(data) {
+    
+    socket.emit('new object', { geometry: data.mesh.geometry, material: data.mesh.material });
+  });
+  socket.on('disconnect', function(data) {
+    
+  });
+  
+  // Movement Events
+  socket.on('forward', function(data) {
+    
+  });
+  socket.on('backward', function(data) {
+    
+  });
+  socket.on('left', function(data) {
+    
+  });
+  socket.on('right', function(data) {
+    
+  });
+  socket.on('jump', function(data) {
+    
+  });
+}
