15ef47d208d4616203cbdbf57f576aa96d870021
Noah <noahcoetsee@gmail.com>
2017-09-14 21:14:27 -0400

    Added most updates
    Still need to fix disconnect event.

--- app.js
+++ app.js
@@ -25,26 +25,30 @@ function setupEvents(socket) {
   // Connection Events
   socket.on('new player', function(data) {
     
-    socket.emit('new object', { geometry: data.mesh.geometry, material: data.mesh.material });
-  });
-  socket.on('disconnect', function(data) {
+    for(var i = 0; i < objects.length; i++) {
+      socket.emit('new object', { geometry: objects[i].geometry, material: objects[i].material });
+    }
     
+    socket.broadcast.emit('new object', { geometry: data.mesh.geometry, material: data.mesh.material });
+  });
+  socket.on('disconnect', function() {
+    objects.splice(objects.indexOf()); // FIX
   });
   
   // Movement Events
   socket.on('forward', function(data) {
-    
+    socket.broadcast.emit('update object', { changePosition: { axis: z, value: 10} });
   });
   socket.on('backward', function(data) {
-    
+    socket.broadcast.emit('update object', { changePosition: { axis: z, value: -10} });
   });
   socket.on('left', function(data) {
-    
+    socket.broadcast.emit('update object', { changePosition: { axis: x, value: -10} });
   });
   socket.on('right', function(data) {
-    
+    socket.broadcast.emit('update object', { changePosition: { axis: x, value: 10} });
   });
   socket.on('jump', function(data) {
-    
+    socket.broadcast.emit('update object', { changePosition: { axis: y, value: 15} });
   });
 }
